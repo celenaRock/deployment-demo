49a22e48a4df52222f800695c4906a71e486e323
Celena Rock <celena.r.rock@gmail.com>
2021-11-17 12:00:10 -0600

    initial

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '../index.html'))
+});
+
+const port = process.env.PORT || 4005;
+
+app.listen(port, () => {
+    console.log(`My app is JAMMIN on port numbero ${port}`);
+});
+
